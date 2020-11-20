8f54b894b26a921c6188e4b878a9da49c9081e2f
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-20 10:43:37 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -3,10 +3,10 @@ let listComment=document.getElementById("list");
 let inputComment=document.getElementById("comment-input");
 let counter=document.getElementById("counter");
 let pauseButton=document.getElementById("pause");
-let result=0;
 let minusButton=document.getElementById("minus");
 let plusBitton=document.getElementById("plus");
 let heartButton=document.getElementById("heart");
+let result=0;
 let internalId;
 
 
