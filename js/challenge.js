f966609cb218ee922c4773710981d007bfafafe3
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-20 11:21:50 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -56,7 +56,7 @@ internalId=setInterval(counterIncrement, 1000);
 
 //heart button//
 heartButton.addEventListener("click", function(){
-  //clearTimeout(internalId);
+  clearTimeout(internalId);
   liketList.innerHTML += "<li>"+ counter.innerText +"</li>"
 });
 
