1ec4deb2dc3bf369481cb738a222b99f8614154a
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-20 10:09:33 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,22 +1,26 @@
-
-//submit butten//
 let subButten=document.getElementById("submit");
 let listComment=document.getElementById("list");
 let inputComment=document.getElementById("comment-input");
+let counter=document.getElementById("counter");
+let pauseButten=document.getElementById("pause");
+let result=0;
+
+
+//submit butten//
 subButten.addEventListener("click", function(){
   listComment.innerText=inputComment.value;
 });
 
+
 //counter//
-let counter=document.getElementById("counter");
-let result=0;
 let internalId=setInterval( function(){
       counter.innerText= result++;
     }, 1000);
 
+
 //pause buttons//
-let pauseButten=document.getElementById("pause");
 pauseButten.addEventListener("click", function(){
 clearTimeout(internalId);
+
 });
 
